3bc9c9462dc9ba7e7da3d733ef4d82f3042a32ee
Doğukan Kaplan <160619803+dogukankaplan@users.noreply.github.com>
2025-12-26 01:37:35 +0300

    problems fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Static Assets -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <script src="{{ asset('assets/js/app.js') }}" defer></script>
 
     <!-- Inline Styles for Apple-like Feel if Vite not compiled -->
     <style>
