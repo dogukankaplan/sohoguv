f1565cf5b9bfe463cf623b7f6ddb7062a06dd4e5
Doğukan Kaplan <160619803+dogukankaplan@users.noreply.github.com>
2026-01-05 15:57:28 +0300

    update

--- app/Filament/Resources/ClientResource/Pages/EditClient.php
+++ app/Filament/Resources/ClientResource/Pages/EditClient.php
@@ -16,18 +16,4 @@ class EditClient extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
-
-    protected function handleRecordUpdate(\Illuminate\Database\Eloquent\Model $record, array $data): \Illuminate\Database\Eloquent\Model
-    {
-        // Check if there's an uploaded file in the request
-        if (request()->hasFile('logo')) {
-            $file = request()->file('logo');
-            $filename = $file->store('clients', 'public');
-            $data['logo'] = $filename;
-        }
-
-        $record->update($data);
-
-        return $record;
-    }
 }
